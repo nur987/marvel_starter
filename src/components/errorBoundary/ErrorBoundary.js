001a3383030406ee6e69766b61bf667eb74e7516
Shaydiev Nurzhigit <75260064+nur987@users.noreply.github.com>
2022-02-26 13:24:06 +0600

    optimize code with new created hook and added new component

--- src/components/errorBoundary/ErrorBoundary.js
+++ src/components/errorBoundary/ErrorBoundary.js
@@ -2,24 +2,24 @@ import { Component } from "react";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 
 class ErrorBoundary extends Component {
-    state = {
-        error: false
+  state = {
+    error: false,
+  };
+
+  componentDidCatch(error, errorInfo) {
+    console.log(error, errorInfo);
+    this.setState({
+      error: true,
+    });
+  }
+
+  render() {
+    if (this.state.error) {
+      return <ErrorMessage />;
     }
 
-    componentDidCatch(error, errorInfo) {
-        console.log(error, errorInfo);
-        this.setState({
-            error: true
-        })
-    }
-
-    render() {
-        if (this.state.error) {
-            return <ErrorMessage/>
-        }
-
-        return this.props.children;
-    }
+    return this.props.children;
+  }
 }
 
 export default ErrorBoundary;
