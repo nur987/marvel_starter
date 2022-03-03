c0d65a03c368bbaac8bef6e03157cb7adf338041
Shaydiev Nurzhigit <75260064+nur987@users.noreply.github.com>
2022-03-03 21:35:48 +0600

    optimize with Formik,Field,Form,ErrorMessage

--- src/components/pages/form/Form.js
+++ src/components/pages/form/Form.js
@@ -1,4 +1,88 @@
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+import "./Form.scss";
+
+const CustomForms = () => {
+  return (
+    <Formik
+      initialValues={{
+        name: "",
+        email: "",
+        amount: 0,
+        currency: "",
+        text: "",
+        terms: false,
+      }}
+      validationSchema={Yup.object({
+        name: Yup.string()
+          .min(2, "Минимум 2 символа")
+          .required("Обязательная поле!"),
+        email: Yup.string()
+          .email("Неправильный email адрес")
+          .required("Обязательная поле!"),
+        amount: Yup.number()
+          .min(5, "Не менее 5")
+          .required("Обязательная поле!"),
+        currency: Yup.string().required("Выберите валюту"),
+        text: Yup.string().min(10, "Не менее 10 символов"),
+        terms: Yup.boolean()
+          .required("Необходимо согласие")
+          .oneOf([true], "Необходимо согласие"),
+      })}
+      onSubmit={(values) => console.log(JSON.stringify(values, null, 2))}
+    >
+      <Form className="form">
+        <h2>Отправить пожертвование</h2>
+        <label htmlFor="name">Ваше имя</label>
+        <Field
+          id="name"
+          name="name"
+          type="text"
+          /* value={formik.values.email}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur} */
+          //!Написали вместо этих трех
+        />
+        {/* {formik.errors.terms && formik.touched.terms ? (
+          <div className="error">{formik.errors.terms}</div>
+        ) : null} */}
+        <ErrorMessage className="error" name="name" component="div" />
+        <label htmlFor="email">Ваша почта</label>
+        <input id="email" name="email" type="email" />
+
+        <label htmlFor="amount">Количество</label>
+        <Field id="amount" name="amount" type="number" />
+        <ErrorMessage className="error" name="email" component="div" />
+
+        <label htmlFor="currency">Валюта</label>
+        <Field id="currency" name="currency" as="select">
+          <option value="">Выберите валюту</option>
+          <option value="USD">USD</option>
+          <option value="UAH">UAH</option>
+          <option value="RUB">RUB</option>
+        </Field>
+        <ErrorMessage className="error" name="currency" component="div" />
+        <label htmlFor="text">Ваше сообщение</label>
+        <Field id="text" name="text" />
+        <ErrorMessage className="error" name="text" component="div" />
+
+        <label className="checkbox">
+          <Field name="terms" type="checkbox" as="checkbox" />
+          Соглашаетесь с политикой конфиденциальности?
+        </label>
+        <ErrorMessage className="error" name="terms" component="div" />
+
+        <button type="submit">Отправить</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default CustomForms;
+
+////! library;
+/* import { useFormik } from "formik";
 import * as Yup from "yup";
 
 import "./Form.scss";
@@ -114,7 +198,7 @@ const Forms = () => {
   );
 };
 
-export default Forms;
+export default Forms; */
 
 /*
 //With hook
